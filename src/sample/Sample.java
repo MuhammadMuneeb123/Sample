8a6a8872e10149f3cd27c22cde0a22a8582a7910
Muhammad <muneebsajid@gmail.com>
2019-05-21 14:14:26 -0400

    added content to comment

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -21,7 +21,7 @@ public class Sample {
      */
     public static void main(String[] args) {
         // Write whatever you want and enjoy!!!!!!!
-        public String getColor
+        public String getColor () {
                
       
     }
